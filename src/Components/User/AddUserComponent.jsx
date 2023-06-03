34f4320e97c6b82fb66862631adbd048ba4134b0
Obada Yehia <67144995+obadatech01@users.noreply.github.com>
2023-06-03 22:12:51 +0300

    updated

--- src/Components/User/AddUserComponent.jsx
+++ src/Components/User/AddUserComponent.jsx
@@ -1,6 +1,32 @@
 import React from "react";
+import Spinner from "react-bootstrap/esm/Spinner";
+import AddUserHook from "../../hook/user/add-user-hook";
 
 const AddUserComponent = () => {
+  const [
+    name,
+    email,
+    identificationNumber,
+    phone,
+    whatsapp,
+    address,
+    gender,
+    role,
+    password,
+    loading,
+    isPress,
+    handleSubmit,
+    onChangeName,
+    onChangeEmail,
+    onChangeIdentificationNumber,
+    onChangePhone,
+    onChangeWhatsApp,
+    onChangeAddress,
+    onChangeGender,
+    onChangeRole,
+    onChangePassword,
+  ] = AddUserHook();
+
   return (
     <div className="row small-spacing">
       <div className="col-xs-12">
@@ -11,116 +37,138 @@ const AddUserComponent = () => {
 
           <div className="row">
             <div className="card-content">
-              <from>
-                <div className="col-md-6">
-                  <div className="padding-20">
+              {/* <from> */}
+              <div className="col-md-6">
+                <div className="padding-20">
+                  <h5>
+                    <b>اسم الموظف</b>
+                  </h5>
+                  <input
+                    type="text"
+                    className="form-control"
+                    maxLength={25}
+                    name="name"
+                    id="name"
+                    value={name}
+                    onChange={onChangeName}
+                    placeholder="اسم الموظف"
+                  />
+                  <div className="margin-top-20">
                     <h5>
-                      <b>اسم الموظف</b>
+                      <b>البريد الإلكتروني</b>
+                    </h5>
+                    <input
+                      type="email"
+                      maxLength={25}
+                      name="email"
+                      className="form-control"
+                      id="email"
+                      value={email}
+                      onChange={onChangeEmail}
+                      placeholder="البريد الإلكتروني"
+                    />
+                  </div>
+                  <div className="margin-top-20">
+                    <h5>
+                      <b>رقم الهوية</b>
                     </h5>
                     <input
                       type="text"
-                      className="form-control"
                       maxLength={25}
-                      name="defaultconfig"
-                      id="defaultconfig"
-                      placeholder="اسم الموظف"
+                      name="identificationNumber"
+                      className="form-control"
+                      id="identificationNumber"
+                      value={identificationNumber}
+                      onChange={onChangeIdentificationNumber}
+                      placeholder="رقم الهوية"
                     />
-                    <div className="margin-top-20">
-                      <h5>
-                        <b>البريد الإلكتروني</b>
-                      </h5>
-                      <input
-                        type="email"
-                        maxLength={25}
-                        name="email"
-                        className="form-control"
-                        id="email"
-                        placeholder="البريد الإلكتروني"
-                      />
-                    </div>
-                    <div className="margin-top-20">
-                      <h5>
-                        <b>رقم الهوية</b>
-                      </h5>
-                      <input
-                        type="text"
-                        maxLength={25}
-                        name="thresholdconfig"
-                        className="form-control"
-                        id="thresholdconfig"
-                        placeholder="رقم الهوية"
-                      />
-                    </div>
-                    <div className="margin-top-20">
-                      <h5>
-                        <b>رقم الهاتف</b>
-                      </h5>
-                      <input
-                        type="text"
-                        maxLength={25}
-                        name="thresholdconfig"
-                        className="form-control"
-                        id="thresholdconfig"
-                        placeholder="رقم الهاتف"
-                      />
-                    </div>
-                    <div className="margin-top-20">
-                      <h5>
-                        <b>رقم الواتساب</b>
-                      </h5>
-                      <input
-                        type="text"
-                        maxLength={25}
-                        name="whatsapp"
-                        className="form-control"
-                        id="whatsapp"
-                        placeholder="رقم الواتساب"
-                      />
-                    </div>
+                  </div>
+                  <div className="margin-top-20">
+                    <h5>
+                      <b>رقم الهاتف</b>
+                    </h5>
+                    <input
+                      type="text"
+                      maxLength={25}
+                      name="phone"
+                      className="form-control"
+                      id="phone"
+                      value={phone}
+                      onChange={onChangePhone}
+                      placeholder="رقم الهاتف"
+                    />
+                  </div>
+                  <div className="margin-top-20">
+                    <h5>
+                      <b>رقم الواتساب</b>
+                    </h5>
+                    <input
+                      type="text"
+                      maxLength={25}
+                      name="whatsapp"
+                      className="form-control"
+                      id="whatsapp"
+                      value={whatsapp}
+                      onChange={onChangeWhatsApp}
+                      placeholder="رقم الواتساب"
+                    />
                   </div>
                 </div>
-                <div className="col-md-6">
-                  <div className="padding-20">
+              </div>
+              <div className="col-md-6">
+                <div className="padding-20">
+                  <h5>
+                    <b>الجنس</b>
+                  </h5>
+                  <select
+                    className="form-control select2_1"
+                    value={gender}
+                    onChange={onChangeGender}
+                  >
+                    <optgroup label="نوع الجنس">
+                      <option value="ذكر">ذكر</option>
+                      <option value="أنثى">أنثى</option>
+                    </optgroup>
+                  </select>
+                  <div className="margin-top-20">
                     <h5>
-                      <b>الجنس</b>
+                      <b>العنوان</b>
                     </h5>
-                    <select className="form-control select2_1">
-                      <optgroup label="نوع الجنس">
-                        <option value="ذكر">ذكر</option>
-                        <option value="أنثى">أنثى</option>
-                      </optgroup>
-                    </select>
-                    <div className="margin-top-20">
-                      <h5>
-                        <b>العنوان</b>
-                      </h5>
-                      <input
-                        type="text"
-                        maxLength={25}
-                        name="thresholdconfig"
-                        className="form-control"
-                        id="thresholdconfig"
-                        placeholder="العنوان"
-                      />
-                    </div>
-                    <div className="margin-top-20">
-                      <h5>
-                        <b>كلمة المرور</b>
-                      </h5>
-                      <input
-                        type="password"
-                        maxLength={25}
-                        name="password"
-                        className="form-control"
-                        id="password"
-                        placeholder="كلمة المرور"
-                      />
-                    </div>
-                    <div className="margin-top-20">
+                    <input
+                      type="text"
+                      maxLength={25}
+                      name="address"
+                      className="form-control"
+                      id="address"
+                      value={address}
+                      onChange={onChangeAddress}
+                      placeholder="العنوان"
+                    />
+                  </div>
+                  <div className="margin-top-20">
+                    <h5>
+                      <b>كلمة المرور</b>
+                    </h5>
+                    <input
+                      type="password"
+                      maxLength={25}
+                      name="password"
+                      className="form-control"
+                      id="password"
+                      value={password}
+                      onChange={onChangePassword}
+                      placeholder="كلمة المرور"
+                    />
+                  </div>
+                  <div className="margin-top-20">
                     <h5>
                       <b>الصلاحية</b>
                     </h5>
-                    <select className="form-control select2_1">
+                    <select
+                      className="form-control select2_1"
+                      value={role}
+                      onChange={onChangeRole}
+                    >
                       <optgroup label="اختر الصلاحية">
                         <option value="">اختر الصلاحية</option>
                         <option value="نائب مدير">نائب مدير</option>
@@ -129,34 +177,28 @@ const AddUserComponent = () => {
                         <option value="محاسب">محاسب</option>
                       </optgroup>
                     </select>
-                    </div>
-                    <div className="margin-top-20">
-                    <div className="box-content">
-                      <h4 className="box-title">قم بتحميل الصورة</h4>
-                      {/* /.box-title */}
-                      <input
-                        type="file"
-                        id="input-file-now-custom-1"
-                        className="dropify"
-                        // onChange={onChangeDocument}
-                        // data-default-file={document}
-                      />
-                    </div>
-                    {/* /.box-content */}
-                    </div>
                   </div>
                 </div>
-                <div className="col-md-12 d-flex justify-content-start">
-                  <button
-                    type="submit"
-                    className="btn mt-4 btn-primary btn-sm waves-effect waves-light"
-                  >
-                    حفظ
-                  </button>
-                </div>
-              </from>
+              </div>
+              <div className="col-md-12 d-flex justify-content-start">
+                <button
+                  type="submit"
+                  onClick={handleSubmit}
+                  className="btn mt-4 btn-primary btn-sm waves-effect waves-light"
+                >
+                  حفظ
+                </button>
+              </div>
+              {/* </from> */}
             </div>
           </div>
+          {isPress ? (
+            loading ? (
+              <Spinner animation="border" variant="primary" />
+            ) : (
+              <h3 className="d-flex justify-content-center">تم الانتهاء</h3>
+            )
+          ) : null}
         </div>
       </div>
     </div>
--- src/hook/user/add-user-hook.js
+++ src/hook/user/add-user-hook.js
@@ -5,15 +5,15 @@ import notify from "../useNotification";
 
 const AddUserHook = () => {
   const dispatch = useDispatch();
-  const defaultDocument = "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg";
   const [name, setName] = useState("");
   const [identificationNumber, setIdentificationNumber] = useState("");
   const [email, setEmail] = useState("");
-  const [profileImg, setProfileImg] = useState("");
-  const [selectedFile, setSelectedFile] = useState(null);
   const [phone, setPhone] = useState("");
   const [whatsapp, setWhatsapp] = useState("");
   const [address, setAddress] = useState("");
+  const [gender, setGender] = useState("ذكر");
+  const [role, setRole] = useState("");
+  const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(true);
   const [isPress, setIsPress] = useState(false);
 
@@ -55,28 +55,39 @@ const AddUserHook = () => {
     setAddress(e.target.value)
   }
 
-  //to change profileImg state
-  const onChangeProfileImg = (e) => {
+  //to change gender state
+  const onChangeGender = (e) => {
     e.persist();
-    if (e.target.files && e.target.files[0]) {
-      console.log(e.target.files[0]);
-      setProfileImg(URL.createObjectURL(e.target.files[0]))
-      setSelectedFile(e.target.files[0]);
-    }
+    setGender(e.target.value)
+  }
+
+  //to change role state
+  const onChangeRole = (e) => {
+    e.persist();
+    setRole(e.target.value)
+  }
+
+  //to change password state
+  const onChangePassword = (e) => {
+    e.persist();
+    setPassword(e.target.value)
   }
 
   // save data in database
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    const formData = new FormData();
-    formData.append("name", name);
-    formData.append("email", email);
-    formData.append("identificationNumber", identificationNumber);
-    formData.append("profileImg", profileImg);
-    formData.append("phone", phone);
-    formData.append("whatsapp", whatsapp);
-    formData.append("address", address);
+    
+    const formData = {
+      name,
+      email,
+      identificationNumber,
+      phone,
+      whatsapp,
+      address,
+      gender,
+      role,
+      password
+    };
 
     setLoading(true);
     setIsPress(true);
@@ -89,11 +100,12 @@ const AddUserHook = () => {
       setName("");
       setEmail("");
       setIdentificationNumber("");
-      setProfileImg(defaultDocument);
-      setSelectedFile(null);
       setPhone("");
       setWhatsapp("");
       setAddress("");
+      setGender("");
+      setRole("");
+      setPassword("");
       setLoading(true);
       setTimeout(() => setIsPress(false), 1000);
       if (res.status === 201) {
@@ -104,7 +116,7 @@ const AddUserHook = () => {
     }
   }, [loading, res]);
 
-  return [name, email, identificationNumber, profileImg, phone, whatsapp, address, loading, isPress, handleSubmit, onChangeName, onChangeEmail, onChangeIdentificationNumber, onChangeProfileImg, onChangePhone, onChangeWhatsApp, onChangeAddress];
+  return [name, email, identificationNumber, phone, whatsapp, address, gender, role, password, loading, isPress, handleSubmit, onChangeName, onChangeEmail, onChangeIdentificationNumber, onChangePhone, onChangeWhatsApp, onChangeAddress, onChangeGender, onChangeRole, onChangePassword];
 };
 
 export default AddUserHook;
