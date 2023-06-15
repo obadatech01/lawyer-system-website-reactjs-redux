39cefc3afef3a8b626652229c648eb492291c794
Obada Yehia <67144995+obadatech01@users.noreply.github.com>
2023-06-15 17:14:01 +0300

    lawyer-system-website-reactjs-redux

--- src/Components/UI/Starter.jsx
+++ src/Components/UI/Starter.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import Footer from '../Utils/Footer';
-import { Link, useLocation } from 'react-router-dom';
-import Sidebar from '../Utils/Sidebar';
-import Navbar from '../Utils/Navbar';
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import Footer from "../Utils/Footer";
+import Navbar from "../Utils/Navbar";
+import Sidebar from "../Utils/Sidebar";
 
 const Starter = (props) => {
   const location = useLocation();
   console.log(location);
-  console.log(location.pathname.startsWith('/clients-profile'));
+  console.log(location.pathname.startsWith("/clients-profile"));
+  const breadcrumbs = [
+    { label: "الرئيسية", to: "/" },
+    { label: "القضايا", to: "#" },
+    { label: "القضايا المكتملة", to: "#", active: true },
+  ];
 
   return (
     <div>
@@ -18,21 +23,17 @@ const Starter = (props) => {
           <div className="box-content">
             <h4 className="box-title">أهلًا وسهلًا بك في برنامج المحامي</h4>
             <ol className="breadcrumb">
-              <li>
-                <Link to={"/"}>
-                  <b className="h4">الرئيسية</b>
-                </Link>
-              </li>
-              <li>
-                <Link to="#">
-                  <b className="h4">القضايا</b>
-                </Link>
-              </li>
-              <li>
-                <Link to="#" className="active">
-                  <b className="h4">القضايا المكتملة</b>
-                </Link>
-              </li>
+              {breadcrumbs.map((item, index) => (
+                <li key={index}>
+                  {item.active ? (
+                    <b className="h4">{item.label}</b>
+                  ) : (
+                    <Link to={item.to}>
+                      <b className="h4">{item.label}</b>
+                    </Link>
+                  )}
+                </li>
+              ))}
             </ol>
           </div>
 
@@ -42,7 +43,7 @@ const Starter = (props) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Starter;
